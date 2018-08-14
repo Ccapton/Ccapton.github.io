8b2bc795cd6be31d237bb3a862227f06d161ef9d
ccapton <chenweibin1125@foxmail.com>
2018-08-14 20:02:23 +0800

    Site updated: 2018-08-14 20:02:22

---
+++ pula/update.php
@@ -0,0 +1,20 @@
+<?php
+
+$version=$_GET["version"];
+$package=$_GET["package"];
+$channel=$_GET["channel"];
+
+if($package=="com.pula.freehire"){
+  if(((int)$version)<1.1){
+      header('Content-type:text/json');     //这句是重点，它告诉接收数据的对象此页面输出的是json数据；
+      $arr = array('hasUpdate'=>true,'isSilent'=>true,'isAutoInstall'=>true,"isIgnorable"=>false,"versionCode"=>1,"versionName"=>"1.1"
+      ,"url"=>"http://ccapton.cn/pula/download.php?psw=123","md5"=>"899ef14ca9597dc10177f093987b095b","size"=>1995973);
+      $json = json_encode($arr);
+      echo $json;
+   }
+}else{
+echo "erro package";
+}
+
+
+?>
